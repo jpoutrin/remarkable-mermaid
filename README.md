74564dec529813f8ff0cfee116c479604c87b20c
Jospeh Barber <joseph.barber@genband.com>
2015-08-10 09:48:28 -0400

    Initial Commit
    Version 0.1.0 Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # remarkable-mermaid
+
+A plugin that uses custom fences in remarkable to create mermaid diagrams inside
+
+## Installation
+
+  npm install mermaid-for-remarkable --save
+
+## Usage
+
+  var mermaidAPI = require('mermaidAPI');
+
+  ''``` sequenceDiagram
+  Alice->>Bob:Hello
+  Bob->>Jim:Hi
+  ```''
+
+## Release History
+
+* 0.1.0 Initial release
---
+++ index.js
@@ -0,0 +1,13 @@
+
+var mermaidAPI = require('mermaidAPI');
+
+module.exports = function mermaidPlugin(md, options) {
+  md.renderer.rules.fence_custom['mermaid'] = function(tokens, index) {
+     var token = tokens[index];
+     var result;
+     mermaidAPI.render('id1', token.content, function(cbResult){
+       result = cbResult;
+     });
+     return result;
+  };
+};
