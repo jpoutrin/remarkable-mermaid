896c34bc9af260821a4ca630f1dd90d61f106502
Jeremie Poutrin <jpoutrin@gmail.com>
2019-02-19 14:27:20 +0100

    Update index.js
    Reverting change

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-var mermaidAPI = require('mermaid').mermaidAPI;;
+var mermaidAPI = require('mermaidAPI');;
 
 module.exports = function mermaidPlugin(md, options) {
   md.renderer.rules.fence_custom['mermaid'] = function(tokens, index) {
